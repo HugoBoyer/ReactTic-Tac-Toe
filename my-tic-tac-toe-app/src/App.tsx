2f2c980be930a638c0a1d842a5f69f79973ed6b6
Hugo <boyer.hugo974@gmail.com>
2025-10-25 01:02:22 +0200

    feat JS ajout d'une function click sur les composant square

--- my-tic-tac-toe-app/src/App.tsx
+++ my-tic-tac-toe-app/src/App.tsx
@@ -1,29 +1,38 @@
 import './App.css'
 
+function Square({value}) {
+  function handleClick() {
+    console.log('clicqué !')
+  }
+
+  return (
+    <button onClick={handleClick} className='square'>
+      {value}
+    </button>
+  ) 
+}
+
 function App() {
   return (
     <>
     <div className="board-row">   
-      <Square />
-      <Square />
-      <Square />
+      <Square value={1} />
+      <Square value={2} />
+      <Square value={3} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={4} />
+      <Square value={5} />
+      <Square value={6} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={7} />
+      <Square value={8} />
+      <Square value={9} />
     </div>
     </>
   )
 }
 
-function Square() {
-  return <button className='square'>1</button>
-}
 
 export default App
