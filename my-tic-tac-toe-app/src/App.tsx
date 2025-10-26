75ebb79d6ef5342a6ff0c06c03961ed331e5984b
Hugo <boyer.hugo974@gmail.com>
2025-10-26 15:22:49 +0100

    feat JS ajout du handleClick qui s'effectue au click

--- my-tic-tac-toe-app/src/App.tsx
+++ my-tic-tac-toe-app/src/App.tsx
@@ -1,34 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 
-function Square({value}) {
-  function handleClick() {
-    console.log('clicqué !')
-  }
-
+function Square({value,onSquareClick}) {
   return (
-    <button onClick={handleClick} className='square'>
+    <button className='square' onClick={onSquareClick}>
       {value}
     </button>
   ) 
 }
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null)) 
+  function handleClick(i) {
+    const nextSquares = squares.slice() // copie le tableau
+    nextSquares[i] = 'X'
+    setSquares(nextSquares) // met a jour l'état
+  }
   return (
     <>
     <div className="board-row">   
-      <Square value={1} />
-      <Square value={2} />
-      <Square value={3} />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className="board-row">
-      <Square value={4} />
-      <Square value={5} />
-      <Square value={6} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className="board-row">
-      <Square value={7} />
-      <Square value={8} />
-      <Square value={9} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>
   )
